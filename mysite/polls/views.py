b3e6de59872d98f5a231d6cc99f197d85daf7754
seojaeoh <seojaeohcode@gmail.com>
2022-09-23 17:52:57 +0900

    commit

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,14 @@
-from re import template
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
         'latest_question_list': latest_question_list,
     }
-    return render(request, 'polls/index.html', context)
+    return render(request, 'C:\djangoStudy\mysite\polls\templates\index.html',context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
